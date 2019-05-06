5b156bd3e848840e6cb19e547654190d0a9a1c4e
Kowalski, Thomas <tkowalski@agtinternational.com>
2019-05-06 17:56:46 +0200

    fix: make pegjs devDependency only

--- index.d.ts
+++ index.d.ts
@@ -1,20 +1,24 @@
-declare function parse(text: string): AST 
+declare function parse(text: string): AST;
 
-export type AST = Sequence | Alternative | Primitive
+export type AST = Sequence | Alternative | Inverse | Primitive;
 
 export interface Sequence {
-  type: 'sequence'
-  paths: AST[]
+  type: "sequence";
+  paths: AST[];
 }
 export interface Alternative {
-  type: 'alternative'
-  paths: AST[]
+  type: "alternative";
+  paths: AST[];
+}
+export interface Inverse {
+  type: "inverse";
+  path: Primitive;
 }
 export interface Primitive {
-  path: string
-  modifier?: Modifier 
+  path: string;
+  modifier?: Modifier;
 }
 export interface Modifier {
-  min: number
-  max: number
+  min: number;
+  max: number;
 }
